d023bc5f9e6e54c5cf745a09fee9a555a58b553e
LukEscopeta <lfcnarcizo73@gmail.com>
2025-02-28 14:52:38 -0300

    calc5

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  num1: number = 0;
+  num2: number = 0;
+  sum: number = 0;
+
+onClickSum(){
+  this.sum = this.num1 + this.num2;
+}
+
+onClickSub(){
+  this.sum = this.num1 - this.num2;
+}
+  
+onClickMul(){
+  this.sum = this.num1 * this.num2;
+}
+
+onClickDiv(){
+  this.sum = this.num1 / this.num2;
+}
+
+onClickPot(){
+  this.sum = this.num1 ** this.num2;
+}
+
+onClickRad(){
+  this.sum = Math.sqrt(this.num1);
+}
 
 }
